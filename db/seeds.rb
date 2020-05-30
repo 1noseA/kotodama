e0e0cc9e78dc3a2cb659f7fe8cc66c061afbc7d0
1noseA <1noseA2L@gmail.com>
2020-05-30 15:22:28 +0900

    [Add]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: "aaa",
+  email: "aa@aa",
+  password: "aaaaaa",
+  status: 1
+)
+User.create!(
+  name: "bbb",
+  email: "bb@bb",
+  password: "bbbbbb",
+  status: 1
+)
+User.create!(
+  name: "ccc",
+  email: "cc@cc",
+  password: "cccccc",
+  status: 1
+)
+User.create!(
+  name: "ddd",
+  email: "dd@dd",
+  password: "dddddd",
+  status: 1
+)
+User.create!(
+  name: "eee",
+  email: "ee@ee",
+  password: "eeeeee",
+  status: 1
+)
+User.create!(
+  name: "fff",
+  email: "ff@ff",
+  password: "ffffff",
+  status: 1
+)
+User.create!(
+  name: "ggg",
+  email: "gg@gg",
+  password: "gggggg",
+  status: 1
+)
+User.create!(
+  name: "hhh",
+  email: "hh@hh",
+  password: "hhhhhh",
+  status: 1
+)
+User.create!(
+  name: "iii",
+  email: "ii@ii",
+  password: "iiiiii",
+  status: 1
+)
+User.create!(
+  name: "jjj",
+  email: "jj@jj",
+  password: "jjjjjj",
+  status: 1
+)
+User.create!(
+  name: "kkk",
+  email: "kk@kk",
+  password: "kkkkkk",
+  status: 1
+)
