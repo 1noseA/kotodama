df24312e85e48d08ad54a58d2d2efa43fb29bb1e
1noseA <1noseA2L@gmail.com>
2020-05-30 10:18:53 +0900

    [Add]user/update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,18 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+      flash[:success] = "プロフィール編集完了！"
+    else
+      render :edit 
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user, :image, :introduction, :status)
   end
 end
