adcecedd21f979fbbd935b7ba0cd9e083f620ad5
1noseA <1noseA2L@gmail.com>
2020-05-31 13:41:40 +0900

    [Add]new-form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  extend Enumerize
+
+  enumerize :category,
+    in: %i[quotations lyrics books comics movies science my someone other], multiple: true
+
+  enumerize :genre,
+    in: %i[love family relationships work school health money myself other], multiple: true
 end
