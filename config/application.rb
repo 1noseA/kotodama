4b74bc5c5d8a07f1dc9719fbf805da8f988bb8f8
1noseA <1noseA2L@gmail.com>
2020-05-31 10:41:06 +0900

    [Add]postテーブル追加と画像アップローダー修正

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Kotodama
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
   end
 end
