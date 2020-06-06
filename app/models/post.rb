f8beab11b54d5c748f2486b8af9ce6f9765d3dc1
1noseA <1noseA2L@gmail.com>
2020-06-06 13:37:09 +0900

    [Fix]いいねの通知を修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -20,13 +20,22 @@ class Post < ApplicationRecord
 
   has_many :notifications, dependent: :destroy
 
-  def create_notification_by(current_user)
-    notification = current_user.active_notifications.new(
-      post_id: id,
-      visited_id: user_id,
-      action: "favorite"
-    )
-    notification.save if notification.valid?
+  def create_notification_favorite!(current_user)
+    # すでに「いいね」されているか検索
+    temp = Notification.where(["visiter_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, user_id, id, 'favorite'])
+    # いいねされていない場合のみ、通知レコードを作成
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        post_id: id,
+        visited_id: user_id,
+        action: 'favorite'
+      )
+      # 自分の投稿に対するいいねの場合は、通知済みとする
+      if notification.visiter_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
   end
 
   def create_notification_comment!(current_user, comment_id)
