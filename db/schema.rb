fb08a5d9caff5eb26d0c5a475adc8d3272350c81
1noseA <1noseA2L@gmail.com>
2020-06-07 12:17:29 +0900

    [Add]paranoia

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_202057) do
+ActiveRecord::Schema.define(version: 2020_06_06_234743) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2020_06_06_202057) do
     t.datetime "updated_at", null: false
     t.integer "status", limit: 1, default: 1, null: false
     t.boolean "admin", default: false
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
