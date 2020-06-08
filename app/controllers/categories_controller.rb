56b2fdf1fee6f145684ae91a019670aa06cbb079
1noseA <1noseA2L@gmail.com>
2020-06-08 23:41:52 +0900

    [Update]カテゴリーページのレイアウト調整

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
-    @posts = Post.where(category: params[:id])
+    @posts = Post.where(category: params[:id]).page(params[:page])
     @categories = Post.category.values
     # 検索
     @q = Post.ransack(params[:q])
