dea7a263dbff93ed866b82e9bb866d92c1935150
1noseA <1noseA2L@gmail.com>
2020-06-10 09:23:00 +0900

    [Add]エピソードタイトルを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -68,7 +68,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:word, :source, :who, :category, :episode, :genre, :status, :user_id)
+    params.require(:post).permit(:word, :source, :who, :category, :title, :episode, :genre, :status, :user_id)
   end
 
   def set_post
