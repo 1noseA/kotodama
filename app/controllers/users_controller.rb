b9c3753966900563b78270af0691261f58411e75
1noseA <1noseA2L@gmail.com>
2020-06-15 12:25:17 +0900

    [Fix]他人のマイページに行けないようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   before_action :authenticate_user!
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy, :show]
 
   def index
     @users = User.page(params[:page])
