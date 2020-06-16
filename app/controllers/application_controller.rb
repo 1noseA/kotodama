eff906d73cba2c8c480ce13c4845da423356013b
1noseA <1noseA2L@gmail.com>
2020-06-16 13:40:09 +0900

    [Fix]ログイン後の遷移先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  # ログイン後に遷移するpathを設定
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
 end
